892d7b39f14018c8db053a8549e7e448d8c45387
Logan <loganlugez8@gmail.com>
2026-08-28 22:40:18 +0200

    init the project

---
+++ header.hpp
@@ -0,0 +1,13 @@
+#ifndef __HEADER_HPP__
+#define __HEADER_HPP__
+
+#define SCREEN_W 1280
+#define SCREEN_H 800
+
+struct Paddle {
+	float x, y;
+	float width;
+	float height;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "raylib.h"
+#include "header.hpp"
+
+// Entities 
+// 	Paddle
+// 	Ball
+// 	Player
+//	Bonus
+
+int main()
+{
+	InitWindow(SCREEN_W, SCREEN_H, "Pong");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		// update
+		float dt = GetFrameTime();
+		ClearBackground(WHITE);
+
+		// draw
+		BeginDrawing();
+			DrawText("Hello World", SCREEN_W / 2, SCREEN_H / 2, 20, GRAY);
+		EndDrawing();
+	}
+}
