457193e0e69a703561b1c7c4a5756cf35a15d549
Logan <loganlugez8@gmail.com>
2026-09-01 16:23:51 +0200

    Allow the resize of the window

--- header.hpp
+++ header.hpp
@@ -1,13 +1,37 @@
 #ifndef __HEADER_HPP__
 #define __HEADER_HPP__
 
-#define SCREEN_W 1280
-#define SCREEN_H 800
+#define FPS 60
 
+#define SCREEN_W 1280.0f
+#define SCREEN_H 900.0f
+
+#define TERRAIN_WIDTH 800.0f
+#define TERRAIN_HEIGHT 400.0f
+
+#define BASE_BALL_SPEED 400.0f
+#define BASE_BALL_RADIUS TERRAIN_HEIGHT / 48
+#define BASE_BALL_X (TERRAIN_WIDTH / 2)
+#define BASE_BALL_Y (TERRAIN_HEIGHT / 2)
+
+#define BASE_PADDLE_SPEED 300.0f
+#define BASE_PADDLE_HEIGHT TERRAIN_HEIGHT / 6
+#define BASE_PADDLE_WIDTH TERRAIN_HEIGHT / 24
+#define BASE_PADDLE_Y TERRAIN_HEIGHT / 2 - (BASE_PADDLE_HEIGHT / 2)
+#define BASE_PADDLE_LEFT_X TERRAIN_WIDTH / 12
+#define BASE_PADDLE_RIGHT_X TERRAIN_WIDTH - BASE_PADDLE_LEFT_X
 struct Paddle {
 	float x, y;
 	float width;
 	float height;
+	float speed;
+};
+
+struct Ball {
+	float x, y;
+	float vx, vy;
+	float radius;
+	float speed;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -10,19 +10,60 @@
 
 int main()
 {
+	// Game init
+	Paddle p_left { BASE_PADDLE_LEFT_X, BASE_PADDLE_Y, BASE_PADDLE_WIDTH, BASE_PADDLE_HEIGHT, BASE_PADDLE_SPEED};
+	Paddle p_right {BASE_PADDLE_RIGHT_X, BASE_PADDLE_Y, BASE_PADDLE_WIDTH, BASE_PADDLE_HEIGHT, BASE_PADDLE_SPEED};
+	Ball b { BASE_BALL_X, BASE_BALL_Y, 0, 0, BASE_BALL_RADIUS};
+
+	// Window init
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
 	InitWindow(SCREEN_W, SCREEN_H, "Pong");
-	SetTargetFPS(60);
+    SetWindowMinSize(320, 240);
+
+	SetTargetFPS(FPS);
+
 
 	while (!WindowShouldClose())
 	{
-		// update
 		float dt = GetFrameTime();
-		ClearBackground(WHITE);
+		int currentScreenWidth = GetScreenWidth();
+		int currentScreenHeight = GetScreenHeight();
+
+		// update
+		float ratio = 0;
+		float offset_x = 0;
+		float offset_y = 0;
+		float ratio_h = currentScreenHeight / TERRAIN_HEIGHT;
+		float ratio_w = currentScreenWidth / TERRAIN_WIDTH;
+		int w_full = 0;
+
+		if (ratio_w < ratio_h)
+		{
+			ratio = ratio_w;
+			offset_y = (currentScreenHeight - TERRAIN_HEIGHT * ratio) / 2;
+			w_full = 1;
+		}
+		else
+		{
+			ratio = ratio_h;
+			offset_x = (currentScreenWidth - TERRAIN_WIDTH * ratio) / 2;
+		}
+
 
 		// draw
 		BeginDrawing();
-			DrawText("Hello World", SCREEN_W / 2, SCREEN_H / 2, 20, GRAY);
-			DrawText("hello", 10, 10, 20, GRAY);
+
+			ClearBackground(WHITE);
+			DrawCircle(b.x * ratio + offset_x, b.y * ratio + offset_y, b.radius * ratio, RED);
+			DrawRectangle(p_left.x * ratio + offset_x, p_left.y * ratio + offset_y, p_left.width * ratio, p_left.height * ratio, BLACK);
+			DrawRectangle(p_right.x * ratio + offset_x, p_right.y * ratio + offset_y, p_right.width * ratio, p_right.height * ratio, BLACK);
+			DrawText(TextFormat("W: %d", currentScreenWidth ), 100, 100, 20, GRAY);
+			DrawText(TextFormat("H: %d", currentScreenHeight), 100, 120, 20, GRAY);
+			DrawText(TextFormat("W FULL: %d", w_full), 100, 140, 20, GRAY);
+			DrawText(TextFormat("Ratio: %f", ratio), 100, 160, 20, GRAY);
+
 		EndDrawing();
 	}
+
+	CloseWindow();
 }
